9b47bba8fdd92c66ca5a92a995c3e24a3ecc4b23
Pigeon377 <892197240@qq.com>
2022-04-22 14:59:45 +0800

    frist commit

---
+++ engine/engine.go
@@ -0,0 +1,20 @@
+package engine
+
+type Engine struct {
+}
+
+func (engin *Engine) Get() {
+
+}
+
+func (engin *Engine) Put() {
+
+}
+
+func (engin *Engine) Delete() {
+
+}
+
+func (engin *Engine) Update() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Dandelion
+
+go 1.18
