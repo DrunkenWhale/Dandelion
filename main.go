3d657ba3792dd7763c0817d06f8d4ec5ed4d016a
Pigeon377 <892197240@qq.com>
2022-04-28 17:00:30 +0800

    test :function 'WriteDBFile' test

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"Dandelion/disk"
-	"Dandelion/skiplist"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	skipList := skiplist.NewSkipList(17)
-	skipList.Put(778, []byte("sda"))
-	skipList.Put(7738, []byte("1"))
-	skipList.Put(72178, []byte("true"))
-	skipList.Put(71378, []byte("114"))
-	skipList.Put(72378, []byte("114"))
-	skipList.Put(73478, []byte("???"))
-	skipList.Put(77568, []byte("&&&"))
-	disk.WriteDBFile("test.txt", skipList.ExportAllElement())
-	f, err := os.OpenFile("test.txt", os.O_WRONLY|os.O_CREATE, 0666)
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(f)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for _, v := range disk.ReadDBFile("test.txt") {
-		fmt.Println(v.Key, "==>", string(v.Value))
-	}
 
 }
---
+++ test/diskoperation_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"Dandelion/disk"
+	"Dandelion/skiplist"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestWriteDBFile(t *testing.T) {
+	skipList := skiplist.NewSkipList(17)
+	skipList.Put(778, []byte("sda"))
+	skipList.Put(7738, []byte("1"))
+	skipList.Put(72178, []byte("true"))
+	skipList.Put(71378, []byte("114"))
+	skipList.Put(72378, []byte("114"))
+	skipList.Put(73478, []byte("???"))
+	skipList.Put(77568, []byte("&&&"))
+	disk.WriteDBFile("test.txt", skipList.ExportAllElement())
+	f, err := os.OpenFile("test.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, v := range disk.ReadDBFile("test.txt") {
+		t.Log(v.Key, "==>", string(v.Value))
+	}
+}
