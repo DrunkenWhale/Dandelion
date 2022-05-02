cdaef74119adff031eb1cbf48441b93d3259fc24
Pigeon377 <892197240@qq.com>
2022-05-02 22:49:17 +0800

    test: add lsmtree_test.go

--- main.go
+++ main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"Dandelion/lsm"
-	"fmt"
-	"log"
-	"math/rand"
-	"strconv"
-)
-
 func main() {
-	l := lsm.NewLSM()
-	for _, i := range rand.Perm(114) {
-		err := l.Put(i, []byte(strconv.Itoa(i)))
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-	for _, i := range rand.Perm(114) {
-		if values, ok := l.Get(i); ok {
-			fmt.Println(i, string(values))
-		}
-	}
-	fmt.Println(l.Get(114514))
 }
