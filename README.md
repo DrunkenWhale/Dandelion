2e3cbaec10da4677f9ffcf95353aa92699767611
Pigeon377 <892197240@qq.com>
2022-05-03 15:57:15 +0800

    docs: add README && test: update lsmtree_test.go

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # Dandelion
 
-an easy kv storage engine based on lsm
+an easy, delicate kv storage engine based on lsm
+
+## use with golang
+
+```go
+
+package main
+
+import (
+	"Dandelion/lsm"
+	"Dandelion/sstable"
+	"log"
+	"math/rand"
+	"strconv"
+)
+
+func main() {
+	l := lsm.NewLSM()
+	for _, i := range rand.Perm(1145147) {
+		err := l.Put(i, []byte(strconv.Itoa(i)))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	err := sstable.MergeLevel0File()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	values, ok := l.Get(114)
+	log.Println(string(values), ok)
+	// output 114, true
+	err = l.Update(114, []byte("114514"))
+	err = l.Delete(114)
+	values, ok = l.Get(114)
+	//output nil, false
+	log.Println(string(values), ok)
+}
+```
--- test/lsmtree_test.go
+++ test/lsmtree_test.go
@@ -34,8 +34,8 @@ func TestLSMTree2(t *testing.T) {
 			log.Fatalln(err)
 		}
 	}
-	log.Println(l.Update(114514, []byte("test succeed")))
-	log.Println(l.Get(114514))
+	t.Log(l.Update(114514, []byte("test succeed")))
+	t.Log(l.Get(114514))
 }
 
 func TestLSMTree3(t *testing.T) {
@@ -51,5 +51,27 @@ func TestLSMTree3(t *testing.T) {
 		log.Fatalln(err)
 	}
 	values, ok := l.Get(114)
+	t.Log(string(values), ok)
+}
+
+func TestLSMTree4(t *testing.T) {
+	l := lsm.NewLSM()
+	for _, i := range rand.Perm(1145147) {
+		err := l.Put(i, []byte(strconv.Itoa(i)))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	err := sstable.MergeLevel0File()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	values, ok := l.Get(114)
+	t.Log(string(values), ok)
+	// output 114, true
+	err = l.Update(114, []byte("114514"))
+	err = l.Delete(114)
+	values, ok = l.Get(114)
+	//output nil, false
 	t.Log(string(values), ok)
 }
